3eef8b1cb9c617326235441556259b222b4b48d1
Mike Bronner <mike@genealabs.com>
2021-02-07 13:50:42 -0700

    Update LikeCounter.php

--- src/LikeCounter.php
+++ src/LikeCounter.php
@@ -13,7 +13,7 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
 class LikeCounter extends Eloquent implements OverridableModel
 {
     use Overridable;
-    
+
     protected $table = 'likeable_like_counters';
     public $timestamps = false;
     protected $fillable = ['likeable_id', 'likeable_type', 'count'];
@@ -36,16 +36,17 @@ class LikeCounter extends Eloquent implements OverridableModel
         if (empty($modelClass)) {
             throw new \Exception('$modelClass cannot be empty/null. Maybe set the $morphClass variable on your model.');
         }
-        
-        $builder = Like::query()
+
+        $likeModel = Like::model();
+        $builder = $likeModel::query()
             ->select(\DB::raw('count(*) as count, likeable_type, likeable_id'))
             ->where('likeable_type', $modelClass)
             ->groupBy('likeable_id');
-        
+
         $results = $builder->get();
-        
+
         $inserts = $results->toArray();
-        
+
         \DB::table((new static)->table)->insert($inserts);
     }
 }
